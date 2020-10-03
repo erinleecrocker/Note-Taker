c25f9c358638a13265252ae9ad23004e11bfb4a1
Erin Crocker <erindowns1@me.com>
2020-10-02 23:28:38 -0400

    fixed syntax

--- server.js
+++ server.js
@@ -1,94 +1,83 @@
-const express = require("express");
+var express = require("express");
 const fs = require("fs");
-const uuid = require("uuid");
-//build instance 
-const app = express();
-// add a port
-var PORT = process.env.PORT || 3001;
+const path = require("path");
+// generate ids
 const { v4: uuidv4 } = require("uuid");
+// creates an "express" instance
+var app = express();
+// Sets an initial port and allows heroku
+var PORT = process.env.PORT || 8080;
 
-// add middleware
-app.use(express.urlencoded({ extended: true}));
+// Middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./public"));
-// add routes
-// get notes page
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-// get index
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
-// get api
-app.get("/api/notes", (req,res) => {
-    fs.readFile("./db/db.json", "utf-8", (err,data) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).json({
-                error: true,
-                data: null,
-                message: "Unable to retrieve notes.",
-            });
-        }
-        res.json({
-            error: false,
-            data: JSON.parse(data),
-            message: "Sucessfully retrived notes",
-        });
-    });
-});
+// css connect
+app.use(express.static("public"));
 
-// post 
-app.post("/api/notes", (req,res) => {
-    req.body.id = uuidv4();
-    if(!req.body.title || !req.body.text){
-        res.status(400).jason({
-            error: true,
-            data: null,
-            message: "Invalid note. Please reformat and try again.",
-        });
-    fs.readFile("./db/db.json", "utf-8", (err, data) => {
-        if(err) {
-            console.log(err);
-            return res.json({
-                error: true,
-                data: null,
-                message: "Unable to retrieve notes."
-            });
-        }
-        console.log(data);
-        const newNote = JSON.parse(data);
-        newNote.push(req.body);
-        console.log(newNote);
-    fs.writeFile("./db/db.json", JSON.stringify(newNote),(err) => {
-            res.sendFile(path.join(__dirname, "./public/notes.html"));
-            if (err) throw err;
-            res.json({
-                error: true,
-                data: null,
-                message: "Sucessfully added new note.",
-            });
-        });
-    });
-};
-// delete
-app.delete("/api/notes/:id", (req, res) => {
+// Api routes
+  // GET  api
+
+app.get("/api/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf-8", (err, data) => {
       if (err) throw err;
-        const updatedData = JSON.parse(data);
-        const filteredNotes = updatedData.filter(
-            (note) => req.params.id !== note.id
-      );
-      fs.writeFile("./db/db.json", JSON.stringify(filteredNotes, null, 2), (err) => {
+      const updatedData = JSON.parse(data);
+      res.json(updatedData);
+    });
+});
+
+  // POST 
+
+app.post("/api/notes", (req, res) => {
+    // id
+    req.body.id = uuidv4();
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+      if (err) throw err;
+      const updatedData = JSON.parse(data);
+      updatedData.push(req.body);
+      // write it to html file
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(updatedData, null, "\t"),
+        (err) => {
           if (err) throw err;
           res.sendFile(path.join(__dirname, "./public/notes.html"));
         }
       );
     });
-  });
+});
+  // DELETE 
+app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+      if (err) throw err;
+      const updatedData = JSON.parse(data);
+      const filteredNotes = updatedData.filter(
+        (note) => req.params.id !== note.id
+      );
+      // write to database 
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(filteredNotes, null, 2),
+        (err) => {
+          if (err) throw err;
+          res.sendFile(path.join(__dirname, "./public/notes.html"));
+        }
+      );
+    });
+});
+//HTML ROUTE for notes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.listen(PORT, () =>{
-    console.log(`app is running on http://localhost:${PORT}`);
-})
+//HTML ROUTE for core
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// LISTENER
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on http://localhost:${PORT}`);
+});
